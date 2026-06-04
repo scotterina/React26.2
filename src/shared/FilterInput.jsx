ec2e3c57c002fdf66f752225ea1ab6d19c9cda4e
Erin Scott <212621843+scotterina@users.noreply.github.com>
2026-06-03 20:31:03 -0400

    lesson 08

---
+++ src/shared/FilterInput.jsx
@@ -0,0 +1,17 @@
+function FilterInput({ filterTerm, onFilterChange }) {
+  return (
+    <div>
+      <label htmlFor="filterInput">Search Todos:</label>
+
+      <input
+        id="filterInput"
+        type="text"
+        value={filterTerm}
+        onChange={(e) => onFilterChange(e.target.value)}
+        placeholder="Search by title..."
+      />
+    </div>
+  );
+}
+
+export default FilterInput;
